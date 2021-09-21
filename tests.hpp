baf00465e04d3a28437c0573ffe3ebebe1bcb5e6
Roman Bitetti <47545887+romanbtt@users.noreply.github.com>
2021-09-21 15:44:32 -0400

    version with test, tests not done

---
+++ tests.hpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romanbtt <marvin@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 18:57:06 by romanbtt          #+#    #+#             */
+/*   Updated: 2021/09/20 16:27:42 by romanbtt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+#include "./containers/vector.hpp"
+
+#ifndef _COLORS_
+#define _COLORS_
+
+/* FOREGROUND */
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[1;31m"
+#define KGRN  "\x1B[1;32m"
+#define KYEL  "\x1B[1;33m"
+#define KBLU  "\x1B[1;34m"
+#define KMAG  "\x1B[1;35m"
+#define KCYN  "\x1B[1;36m"
+#define KWHT  "\x1B[1;37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define UNDL(x) "\x1B[4m" x RST
+
+#endif  /* _COLORS_ */
+
+void    compareFilesOutput( void );
+
+bool    testVectorConstructors( void );
+
+void    testVectorConstructorAll(void);
+void    testVectorDefaultConstructor(bool isStd, std::string ns, bool all);
+void	testVectorFillConstructor(bool isStd, std::string ns, bool all);
+void	testVectorRangeConstructor(bool isStd, std::string ns, bool all);
+void	testVectorCopyConstructor(bool isStd, std::string ns, bool all);
+void	testVectorOperatorEqual(bool isStd, std::string ns, bool all);
+
+
+bool	testVectorIterators(void);
+
+void    testVectorIteratorAll( void );
+void    testVectorIteratorBegin( bool isStd, std::string ns, bool all );
+void    testVectorIteratorEnd( bool isStd, std::string ns, bool all );
+void    testVectorIteratorRbegin( bool isStd, std::string ns, bool all );
+void    testVectorIteratorRend( bool isStd, std::string ns, bool all );
+
+#endif
---
+++ tests/vectorConstructors.cpp
@@ -0,0 +1,250 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vectorConstructors.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romanbtt <marvin@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 16:16:51 by romanbtt          #+#    #+#             */
+/*   Updated: 2021/09/20 17:59:56 by romanbtt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../tests.hpp"
+
+static void printVectorConstructorsMenu( void )
+{
+    std::cout << FMAG("--- Vector Constructor menu ---") << std::endl;
+    std::cout << FCYN("1") << FWHT(" - Default constructor") << std::endl;
+    std::cout << FCYN("2") << FWHT(" - Fill constructor") << std::endl;
+    std::cout << FCYN("3") << FWHT(" - Range constructor") << std::endl;
+    std::cout << FCYN("4") << FWHT(" - Copy constructor") << std::endl;
+    std::cout << FCYN("5") << FWHT(" - Operator =") << std::endl;
+    std::cout << FGRN("A") << FWHT(" - All") << std::endl;
+    std::cout << FYEL("B") << FWHT(" - Back") << std::endl;
+    std::cout << FRED("E") << FWHT(" - Exit") << std::endl;
+    std::cout << FBLU("Select a test : ");
+}
+
+bool    testVectorConstructors( void )
+{
+    std::string input;
+
+    printVectorConstructorsMenu();
+    while (std::getline(std::cin, input))
+    {
+        system("clear");
+        if (!input.compare("1"))
+            testVectorDefaultConstructor(true, "std", false);
+        else if (!input.compare("2"))
+            testVectorFillConstructor(true, "std", false);
+        else if (!input.compare("3"))
+            testVectorRangeConstructor(true, "std", false);
+        else if (!input.compare("4"))
+            testVectorCopyConstructor(true, "std", false);
+        else if (!input.compare("5"))
+            testVectorOperatorEqual(true, "std", false);
+        else if (!input.compare("A") || !input.compare("a"))
+            testVectorConstructorAll();
+        else if (!input.compare("B") || !input.compare("b"))
+            return (false);
+        else if (!input.compare("E") || !input.compare("e"))
+            return (true);
+        else
+            std::cout << FRED("Command not found!") << std::endl << std::endl;
+        printVectorConstructorsMenu();
+    }
+    return (true);
+}
+
+void    testVectorConstructorAll( void )
+{
+    testVectorDefaultConstructor(true, "std", true);
+    testVectorFillConstructor(true, "std", true);
+    testVectorRangeConstructor(true, "std", true);
+    testVectorCopyConstructor(true, "std", true);
+    testVectorOperatorEqual(true, "std", true);
+}
+
+void    testVectorDefaultConstructor( bool isStd, std::string ns, bool all )
+{
+	#undef NAMESPACE
+    #define NAMESPACE ft
+    if (isStd)
+    {
+        #undef NAMESPACE
+        #define NAMESPACE std
+        std::cout << FBLU("--- Test Default Constructor ---\n") << std::endl;
+    }
+
+    std::ofstream testFile(ns.c_str(), std::ios::out | std::ios::trunc);
+    NAMESPACE::vector<int> test;
+
+    if (!all)
+    {
+        std::cout << "Default constructor namespace : " << ns << std::endl;
+        std::cout << "Capacity of " << ns << " vector : " << test.capacity() << std::endl;
+        std::cout << "Size of " << ns << " vector : " << test.size() << std::endl;
+        std::cout << "Content of " << ns << " vector :" << std::endl;
+        for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+            std::cout << "| " << *it << " |"; 
+        std::cout << std::endl;
+    }
+    testFile << test.capacity() << "\n";
+    testFile << test.size() << "\n";
+    for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+        testFile << "| " << *it << " |";
+    testFile.close();
+    if (isStd == true)
+        testVectorDefaultConstructor(false, "ft", all);
+    else
+        compareFilesOutput();
+    
+}
+
+void	testVectorFillConstructor( bool isStd, std::string ns, bool all )
+{
+	#undef NAMESPACE
+	#define NAMESPACE ft
+    if (isStd)
+    {
+        #undef NAMESPACE
+        #define NAMESPACE std
+        std::cout << FBLU("--- Test Fill Constructor ---\n") << std::endl;
+    }
+
+    std::ofstream testFile(ns.c_str(), std::ios::out | std::ios::trunc);
+	NAMESPACE::vector<int> test(10, 42);
+
+    if (!all)
+    {
+        std::cout << "Fill constructor namespace : " << ns << std::endl;
+        std::cout << "Capacity of " << ns << " vector : " << test.capacity() << std::endl;
+        std::cout << "Size of " << ns << " vector : " << test.size() << std::endl;
+        std::cout << "Content of " << ns << " vector :" << std::endl;
+        for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+            std::cout << "| " << *it << " |"; 
+        std::cout << std::endl << std::endl;
+    }
+    testFile << test.capacity() << "\n";
+    testFile << test.size() << "\n";
+    for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+        testFile << "| " << *it << " |";
+    testFile.close();
+    if (isStd == true)
+        testVectorFillConstructor(false, "ft", all);
+    else
+        compareFilesOutput();
+}
+
+void	testVectorRangeConstructor( bool isStd, std::string ns, bool all )
+{
+	#undef NAMESPACE
+	#define NAMESPACE ft
+    if (isStd)
+    {
+        #undef NAMESPACE
+        #define NAMESPACE std
+        std::cout << FBLU("--- Test Range Constructor ---\n") << std::endl;
+    }
+
+    std::ofstream testFile(ns.c_str(), std::ios::out | std::ios::trunc);
+	NAMESPACE::vector<int> range(10, 84);
+	NAMESPACE::vector<int> test(range.begin(), range.end() - 5);
+
+    if (!all)
+    {
+        std::cout << "Range constructor namespace : " << ns << std::endl;
+        std::cout << "Capacity of " << ns << " vector : " << test.capacity() << std::endl;
+        std::cout << "Size of " << ns << " vector : " << test.size() << std::endl;
+        std::cout << "Content of " << ns << " vector :" << std::endl;
+        for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+            std::cout << "| " << *it << " |"; 
+        std::cout << std::endl << std::endl;
+    }
+    testFile << test.capacity() << "\n";
+    testFile << test.size() << "\n";
+    for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+        testFile << "| " << *it << " |";
+    testFile.close();
+    if (isStd == true)
+        testVectorRangeConstructor(false, "ft", all);
+    else
+        compareFilesOutput();
+}
+
+void	testVectorCopyConstructor( bool isStd, std::string ns, bool all )
+{
+	#undef NAMESPACE
+	#define NAMESPACE ft
+    if (isStd)
+    {
+        #undef NAMESPACE
+        #define NAMESPACE std
+        std::cout << FBLU("--- Test Copy Constructor ---\n") << std::endl;
+    }
+
+    std::ofstream testFile(ns.c_str(), std::ios::out | std::ios::trunc);
+	NAMESPACE::vector<int> original(10, 1);
+	for (int i = 0; i < original.size(); i++)
+		original[i] += i;
+	NAMESPACE::vector<int> test(original);
+
+    if (!all)
+    {
+        std::cout << "Copy constructor namespace : " << ns << std::endl;
+        std::cout << "Capacity of " << ns << " vector : " << test.capacity() << std::endl;
+        std::cout << "Size of " << ns << " vector : " << test.size() << std::endl;
+        std::cout << "Content of " << ns << " vector :" << std::endl;
+        for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+            std::cout << "| " << *it << " |"; 
+        std::cout << std::endl << std::endl;
+    }
+    testFile << test.capacity() << "\n";
+    testFile << test.size() << "\n";
+    for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+        testFile << "| " << *it << " |";
+    testFile.close();
+    if (isStd == true)
+        testVectorCopyConstructor(false, "ft", all);
+    else
+        compareFilesOutput();
+}
+
+void	testVectorOperatorEqual( bool isStd, std::string ns, bool all )
+{
+	#undef NAMESPACE
+	#define NAMESPACE ft
+    if (isStd)
+    {
+        #undef NAMESPACE
+        #define NAMESPACE std
+        std::cout << FBLU("--- Test Operator Equal ---\n") << std::endl;
+    }
+
+    std::ofstream testFile(ns.c_str(), std::ios::out | std::ios::trunc);
+	NAMESPACE::vector<int> original(7, 1);
+	for (int i = 0; i < original.size(); i++)
+		original[i] += i * 2;
+	NAMESPACE::vector<int> test = original;
+
+    if (!all)
+    {
+        std::cout << "Operator = namespace : " << ns << std::endl;
+        std::cout << "Capacity of " << ns << " vector : " << test.capacity() << std::endl;
+        std::cout << "Size of " << ns << " vector : " << test.size() << std::endl;
+        std::cout << "Content of " << ns << " vector :" << std::endl;
+        for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+            std::cout << "| " << *it << " |"; 
+        std::cout << std::endl << std::endl;
+    }
+    testFile << test.capacity() << "\n";
+    testFile << test.size() << "\n";
+    for (NAMESPACE::vector<int>::iterator it = test.begin(); it != test.end(); it++)
+        testFile << "| " << *it << " |";
+    testFile.close();
+    if (isStd == true)
+        testVectorOperatorEqual(false, "ft", all);
+    else
+        compareFilesOutput();
+}
